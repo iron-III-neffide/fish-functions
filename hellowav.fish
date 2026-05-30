5940ff19ba8427c4868e387aa89e2dd8a15beb78
Noah Hauser <noahhauser42@gmail.com>
2026-05-30 23:42:13 +0200

    better_hellowav

--- hellowav.fish
+++ hellowav.fish
@@ -1,6 +1,8 @@
 function hellowav
     set input (wl-paste)
-    set output $HOME/Music/Exports/(basename $input .wav).mp3
+    set basename (basename $input .wav)
+    set name (if test (count $argv) -gt 0; echo $argv[1]; else; echo $basename; end)
+    set output $HOME/Music/Exports/$name.mp3
     ffmpeg -i $input -codec:a libmp3lame -b:a 192k $output
-    nautilus $output
+    echo "file://$output" | wl-copy --type text/uri-list
 end
