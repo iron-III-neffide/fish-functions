ddbb9a6bc1c980b2d2c49243f8ed6d3ba24729d6
Noah Hauser <noahhauser42@gmail.com>
2026-06-03 14:05:58 +0200

    try_on_artemisf

--- artemis.fish
+++ artemis.fish
@@ -1,4 +1,9 @@
 function artemis
-
-
+cd "/home/noah/CLionProjects/Artemis Template"
+for i in *.c; rm $i; end
+cd $HOME/tmp
+git clone $argv[1]
+cd $argv[1]
+for i in *.c; mv $i "/home/noah/CLionProjects/Artemis Template/i"
+end
 end
