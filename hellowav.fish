06334f146d4da37cf5e70aeb00e555d8831b1d7f
Noah Hauser <noahhauser42@gmail.com>
2026-06-11 18:44:18 +0200

    hellowav_fix

--- hellowav.fish
+++ hellowav.fish
@@ -4,5 +4,5 @@ function hellowav
     set name (if test (count $argv) -gt 0; echo $argv[1]; else; echo $basename; end)
     set output $HOME/Music/Exports/$name.mp3
     ffmpeg -i $input -codec:a libmp3lame -b:a 192k $output
-    nautilus $output
+    echo "file://$output" | wl-copy --type text/uri-list
 end
